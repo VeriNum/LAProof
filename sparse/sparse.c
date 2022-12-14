89a02605322dd36503fc2bddc59f08ee19d21623
Andrew W. Appel <appel@princeton.edu>
2022-12-14 11:49:25 -0500

    Cleanup in sparse.c, more accurate timing in example.c

--- sparse/sparse.c
+++ sparse/sparse.c
@@ -8,65 +8,6 @@ void *surely_malloc(size_t n) {
   return p;
 }
 
-double *sparse2dense (struct sparsevec *v) {
-  unsigned i, j, k=v->k, n = v->n;
-  unsigned *index = v->index;
-  double *val = v->val;
-  double *p = surely_malloc(n * sizeof(double));
-  for (j=0; j<n; j++) p[j]=0.0;
-  for (i=0; i<k; i++) {
-    double x=val[i];
-    j=index[i];
-    p[j]=x;
-  }
-  return p;
-}
-
-struct sparsevec *dense2sparse (double *p, unsigned n) {
-  double *val; unsigned *index; struct sparsevec *v;
-  unsigned i=0, j, k=0;
-  for (j=0; j<n; j++) k += (p[j]!=0.0);
-  val = surely_malloc(k * sizeof(double));
-  index=surely_malloc(k * sizeof(unsigned));
-  for (j=0; j<n; j++) {
-    double x = p[j];
-    if (x!=0.0) {
-      index[i]=j;
-      val[i]=x;
-      i++;
-    }
-  }
-  v = surely_malloc (sizeof (struct sparsevec));
-  v->index=index;
-  v->val=val;
-  v->k=k;
-  v->n=n;
-  return v;
-}
-
-/*  sparsedotprod(v1,v2) multiplies sparse vector v1 with dense vector v2 */
-double sparsedotprod (struct sparsevec *vec1, double *vec2) {
-  double result=0.0;
-  unsigned i, k=vec1->k;
-  unsigned *index = vec1->index;
-  double *val = vec1->val;
-  for (i=0; i<k; i++) {
-    unsigned j = index[i];
-    double x = val[i];
-    double y = vec2[j];
-    result = fma(x,y,result);
-  }
-  return result;
-}
-
-double densedotprod(double *vec1, double *vec2, unsigned n) {
-  double result=0.0;
-  unsigned i;
-  for (i=0; i<n; i++)
-    result = fma(vec1[i],vec2[i],result);
-  return result;
-}
-
 /* crs_matrix_vector_multiply(m,v,p)
       multiplies a sparse matrix m by a dense vector v,
       putting the result into the (already allocated) dense vector p
@@ -97,11 +38,11 @@ void crs_matrix_vector_multiply (struct crs_matrix *m, double *v, double *p) {
    to dimension of D.  let m represent A.
    Then diag_mult(diag,m) sets m to represent D*A */
 void diag_mult(double *diag, struct crs_matrix *m) {
-  unsigned i, rows=m->rows;
-  for (i=0; i<row; i++) {
-    unsigned k=m->row_ptr(i+1);
+  unsigned i, h, rows=m->rows;
+  for (i=0; i<rows; i++) {
+    unsigned k=m->row_ptr[i+1];
     unsigned x = diag[i];
-    for (h=m->row_ptr(i); h<k; h++)
+    for (h=m->row_ptr[i]; h<k; h++)
       m->val[h] *= x;
   }
 }
